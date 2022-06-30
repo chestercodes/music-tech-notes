8bef131d41b4ac63836521e8d01ada47f79f6919
chester <chestercodes@users.noreply.github.com>
2022-06-30 22:16:38 +0100

    Use subprocess

--- akai-mpx/convert-files/convert.py
+++ akai-mpx/convert-files/convert.py
@@ -1,19 +1,24 @@
 import os
 import glob
+from subprocess import call
 
 print("Convert files...")
 
 root_dir = os.path.dirname(os.path.realpath(__file__))
 
 raw_dir = root_dir + "/files/raw"
-raw_files = glob.glob(raw_dir + "/*")
+raw_files = glob.glob(raw_dir + "/**/*", recursive=True)
 
 out_dir = root_dir + "/files/out"
 if not os.path.exists(out_dir):
     os.mkdir(out_dir)
 
 for raw_file in raw_files:
-    out_path = raw_file.replace(raw_dir, out_dir)
+    if os.path.isdir(raw_file):
+        continue
+    
+    raw_file_name = os.path.basename(raw_file)
+    out_path = out_dir + "/" + raw_file_name
     print(raw_file + " -> " + out_path)
 
     if os.path.exists(out_path):
@@ -22,11 +27,9 @@ for raw_file in raw_files:
     # -ac 1 -> mono
     # -ar 48000 -> 48khz
     # -acodec pcm_s16le -> 16bit
-    extra_args = " -ac 1 -ar 48000 -acodec pcm_s16le " + out_path
-    c = "ffmpeg -i " + raw_file + extra_args
-    print(c)
     try:
-        os.system(c)
+        call(('ffmpeg', '-i', raw_file, '-ac', '1', '-ar', '48000', '-acodec', 'pcm_s16le', out_path))
+
     except Exception as err:
         print("call failed")
         print(err)
